243f0bcc816359d9337c4493e7d7420dc3cd0acb
yanliu1111 <liuyan0504@gmail.com>
2023-05-02 16:16:57 -0600

    feat: first commit

---
+++ Start/Overview/MyConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string response;
+Console.WriteLine("What is your name?");
+response = Console.ReadLine();
+Console.WriteLine($"Hello, {response}!");
+
+OperatingSystem thisOS = Environment.OSVersion;
+Console.WriteLine(thisOS.Platform);
+Console.WriteLine(thisOS.VersionString);
